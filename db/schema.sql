75f7d12dd82d05124aeabf2eaf9e7e486f5c34b2
Vani <vani.karikalan@gmail.com>
2019-06-04 17:56:54 -0700

    basic burger app working

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+### Schema
+DROP DATABASE burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(100) NOT NULL,
+    devoured boolean DEFAULT false,
+	PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ('BLT');
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Chicken Burger');
+
+
+select * from burgers;
